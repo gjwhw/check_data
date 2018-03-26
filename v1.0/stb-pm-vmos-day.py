eb65ef09af8813b97127beee68ad8850b92364f8
gjwhw <gjw1989@outlook.com>
2018-03-26 11:34:25 +0800

    规范了变量的命名

--- v1.0/stb-pm-vmos-day.py
+++ v1.0/stb-pm-vmos-day.py
@@ -23,13 +23,13 @@ name1 = ['TIMESTAMP', '省地区代码', '市地区代码', 'STB_ID', '机顶盒
         'MDI-DF平均值', 'MDI-MLR平均值', 'RTP丢包率平均值', 'MDI-DF劣化百分比', 'MDI-MLR劣化百分比',
         'RTP丢包率劣化百分比', '卡顿总时长', '收视总时长', '卡顿总次数', '卡顿超过10秒的总次数', '收视vmos']
 # 通过DataFrame函数导入文件，iterator=True 用迭代的方式
-x = pd.read_csv(filename, iterator=True, names=name1, sep='|', header=None)
+stb_pm_vmos = pd.read_csv(filename, iterator=True, names=name1, sep='|', header=None)
 
 name2 = ['TIMESTAMP', '省地区代码', '市地区代码', 'STB_ID', '机顶盒IP', '业务账号', '接入方式', '机顶盒SN序列号',
 		 '机顶盒MAC', '厂商名称', '机顶盒型号', '机顶盒软件版本号', '机顶盒硬件版本号',]
-y2 = pd.read_csv(filename2,iterator=True, names=name2, sep='|', header=None)
-y1 = y2.get_chunk()
-y1 = y1.loc[:,['STB_ID','厂商名称', '机顶盒型号']]
+stb_cm = pd.read_csv(filename2,iterator=True, names=name2, sep='|', header=None)
+stb_cm_chunk = stb_cm.get_chunk()
+stb_cm_chunk = stb_cm_chunk.loc[:,['STB_ID','厂商名称', '机顶盒型号']]
 
 # 大文件分割处理，一次取10万行
 loop = True
@@ -38,26 +38,25 @@ chunks = []
 g = 0
 while loop:
     try:
-        chunk1 = x.get_chunk(chunkSize)
+        stb_pm_vmos_chunk = stb_pm_vmos.get_chunk(chunkSize)
         # 第一列时间格式为：‘2018-03-22 00:00：00’，取前13位进行分割，后面实现按小时分组：‘2018-03-22 00’
-        chunk1['TIMESTAMP'] = chunk1['TIMESTAMP'].astype('str').str[:13]
+        stb_pm_vmos_chunk['TIMESTAMP'] = stb_pm_vmos_chunk['TIMESTAMP'].astype('str').str[:13]
 
-        chunk = pd.merge(chunk1, y1, on=['STB_ID'])
+        chunk = pd.merge(stb_pm_vmos_chunk, stb_cm_chunk, on=['STB_ID'])
 
         # 求平均数的指标
-        y = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
+        avg = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
                         'MDI-MLR平均值', 'RTP丢包率平均值', 'MDI-DF劣化百分比', 'MDI-MLR劣化百分比', 'RTP丢包率劣化百分比', '收视vmos'].mean()
         # 小数点保留6位
-        y = y.round(6)
+        avg = avg.round(6)
         # 求和
-        n = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
+        sum = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
 						'收视总时长', '卡顿总次数', '卡顿超过10秒的总次数'].sum()
-        n = n.round(6)
+        sum = sum.round(6)
         # 按照3列进行拼接
-        z = pd.merge(y, n, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
+        z = pd.merge(avg, sum, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
 
         chunks.append(z)
-        starttime3 = datetime.datetime.now()
         g += 1
         print(g)
     except StopIteration:
@@ -66,20 +65,20 @@ while loop:
 # 将分割的块文件进行拼接，忽略列名
 df = pd.concat(chunks, ignore_index=True)
 # 再次求平均值
-y = df.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
+avg = df.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
                         'MDI-MLR平均值', 'RTP丢包率平均值', 'MDI-DF劣化百分比', 'MDI-MLR劣化百分比', 'RTP丢包率劣化百分比', '收视vmos'].mean()
-y = y.round(6)
+avg = avg.round(6)
 # 再次求和
-n = df.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
+sum = df.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
 						'收视总时长', '卡顿总次数', '卡顿超过10秒的总次数'].sum()
-n = n.round(6)
+sum = sum.round(6)
 # 按照3列进行拼接
-z = pd.merge(y, n, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
+file_out = pd.merge(avg, sum, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
 
 # 导出文件，去掉索引，去掉列名
-z.to_csv('temporary_file', index=False, sep='|', header=0)
-x.close()
-y2.close()
+file_out.to_csv('temporary_file', index=False, sep='|', header=0)
+stb_pm_vmos.close()
+stb_cm.close()
 # 重命名文件
 # os.unlink(filename)
 # shutil.move('temporary_file', filename)
--- v1.0/stb-pm-vmos-hour.py
+++ v1.0/stb-pm-vmos-hour.py
@@ -14,51 +14,51 @@ print('开始时间')
 print(starttime)
 
 # 传入文件地址路径
-filename = sys.argv[1]
-filename2 = sys.argv[2]
-# filename = '321.txt'
-# filename2 = '1122.txt'
+# filename = sys.argv[1]
+# filename = sys.argv[2]
+filename = '321.txt'
+filename2 = '1122.txt'
 # 定义列名的列表
 name1 = ['TIMESTAMP', '省地区代码', '市地区代码', 'STB_ID', '机顶盒IP', '业务帐号', '机顶盒SN',
         'MDI-DF平均值', 'MDI-MLR平均值', 'RTP丢包率平均值', 'MDI-DF劣化百分比', 'MDI-MLR劣化百分比',
         'RTP丢包率劣化百分比', '卡顿总时长', '收视总时长', '卡顿总次数', '卡顿超过10秒的总次数', '收视vmos']
 # 通过DataFrame函数导入文件，iterator=True 用迭代的方式
-x = pd.read_csv(filename, iterator=True, names=name1, sep='|', header=None)
-chunk1 = x.get_chunk()
+stb_pm_vmos = pd.read_csv(filename, iterator=True, names=name1, sep='|', header=None)
+stb_pm_vmos_chunk = stb_pm_vmos.get_chunk()
 # 定义列名的列表
 name2 = ['TIMESTAMP', '省地区代码', '市地区代码', 'STB_ID', '机顶盒IP', '业务账号', '接入方式', '机顶盒SN序列号',
 		 '机顶盒MAC', '厂商名称', '机顶盒型号', '机顶盒软件版本号', '机顶盒硬件版本号',]
-y2 = pd.read_csv(filename2,iterator=True, names=name2, sep='|', header=None)
-y1 = y2.get_chunk()
+stb_cm = pd.read_csv(filename2,iterator=True, names=name2, sep='|', header=None)
+stb_cm_chunk = stb_cm.get_chunk()
 # 提取需要的3列数据
-y1 = y1.loc[:,['STB_ID','厂商名称', '机顶盒型号']]
+stb_cm_chunk = stb_cm_chunk.loc[:,['STB_ID','厂商名称', '机顶盒型号']]
 
 # 第一列时间格式为：‘2018-03-22 00:00：00’，取前13位进行分割，后面实现按小时分组：‘2018-03-22 00’
-chunk1['TIMESTAMP'] = chunk1['TIMESTAMP'].astype('str').str[:13]
+stb_pm_vmos_chunk['TIMESTAMP'] = stb_pm_vmos_chunk['TIMESTAMP'].astype('str').str[:13]
 # 按照STB_ID进行拼接
-chunk = pd.merge(chunk1, y1, on=['STB_ID'])
+chunk = pd.merge(stb_pm_vmos_chunk, stb_cm_chunk, on=['STB_ID'])
 
 # 求平均数的指标
-y = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
+avg = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['MDI-DF平均值',
 				'MDI-MLR平均值', 'RTP丢包率平均值', 'MDI-DF劣化百分比', 'MDI-MLR劣化百分比', 'RTP丢包率劣化百分比', '收视vmos'].mean()
 # 小数点保留6位
-y = y.round(6)
+avg = avg.round(6)
 # 求和
-n = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
+sum = chunk.groupby(['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'], as_index=False)['卡顿总时长',
 				'收视总时长', '卡顿总次数', '卡顿超过10秒的总次数'].sum()
-n = n.round(6)
+sum = sum.round(6)
 # 按照3列进行拼接
 print("Iteration is stopped.")
 # 按照3列进行拼接
-z = pd.merge(y, n, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
+file_out = pd.merge(avg, sum, on=['TIMESTAMP', '省地区代码', '市地区代码','厂商名称', '机顶盒型号'])
 
 # 导出文件，去掉索引，去掉列名
-z.to_csv('temporary_file', index=False, sep='|', header=0)
-x.close()
-y2.close()
+file_out.to_csv('temporary_file', index=False, sep='|', header=0)
+stb_pm_vmos.close()
+stb_cm.close()
 # 重命名文件
-os.unlink(filename)
-shutil.move('temporary_file', filename)
+# os.unlink(filename)
+# shutil.move('temporary_file', filename)
 
 print('运行时间')
 endtime = datetime.datetime.now()
